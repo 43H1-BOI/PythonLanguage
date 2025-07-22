8a76f6d24fc570f6f05bffafcacc002952bebe93
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-22 14:47:04 +0530

    Added basics

---
+++ 3_Taking_Inputs.py
@@ -0,0 +1,11 @@
+name = input("Enter Your Name : ")
+age = input("Enter Your Age : ")
+age = int(age) #TypeCasted to Int
+
+print("Your Details : ")
+print("Name = ",name)
+print("Age = ",age)
+
+# Getting Their Types
+print(type(name))
+print(type(age))
