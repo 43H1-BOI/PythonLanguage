0adbffc60cf5a54a7797d5227c0e066ec7c3f888
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-22 14:40:45 +0530

    Added General Printing

---
+++ General_Printing.py
@@ -0,0 +1,14 @@
+
+# For Normal Printing
+print("Hello World !")
+print("Abhishek Yadav this side.")
+
+
+# Printing next to each other
+print("I am currently learning",end=" ")
+print("Android Development")
+
+
+# Printing with Seperators
+print("I Love Fruites like ")
+print("Mango","Apple","Grapes",sep=" , ")
