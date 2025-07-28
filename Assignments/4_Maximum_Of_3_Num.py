a317c550b56620eea8f4559863d30588dce7e66a
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-28 21:59:02 +0530

    Added Assignments

---
+++ Assignments/4_Maximum_Of_3_Num.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter 1st Number : "))
+num2 = int(input("Enter 2nd Number : "))
+num3 = int(input("Enter 3rd Number : "))
+
+max = num1
+
+if(num1 > num2):
+    if(num1 >= num3):
+        max = num1
+    else :
+        max = num3
+else : # num 2 >= num1
+    if(num2 >= num3) :
+        max = num2
+    else :
+        max = num3    
+
+
+print("Maximum = ",max)        
