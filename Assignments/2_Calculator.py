f0fcb816df4acc429a3f4b8e561211d4923328ef
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-07-28 21:39:19 +0530

    Converted if-else to match-case

--- Assignments/2_Calculator.py
+++ Assignments/2_Calculator.py
@@ -2,6 +2,7 @@ num1 = int(input("Enter 1st Value : "))
 num2 = int(input("Enter 2nd Value : "))
 operator = input("Enter Operator (+,-,*,/,%) : ")
 
+''' Using If Else :-
 if(operator == "+") :
     res = num1 + num2 
 elif(operator == "-") :
@@ -14,6 +15,21 @@ elif(operator == "%") :
     res = num1 % num2    
 else :
     res = operator + " is an Invalid Operator"    
+'''
 
+match operator :
+    case '+':
+        res = num1 + num2 
+    case "-":
+        res = num1 - num2
+    case "*" :
+        res = num1 * num2
+    case "/" :
+        res = num1 / num2
+    case "%" :
+        res = num1 % num2 
+    case any :
+        print(operator + " is an Invalid Operator")
+        res = "Not a Valid Operation" 
 
 print("{num1} {operator} {num2} = {res}".format(num1 = num1 ,operator = operator,num2 = num2,res = res))
