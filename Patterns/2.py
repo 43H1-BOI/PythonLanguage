510ed91729faaafb601f56577dbf27a2177bb93e
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-08-09 21:36:03 +0530

    Added pattern for Class

---
+++ Patterns/2.py
@@ -0,0 +1,17 @@
+'''
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * *
+'''
+
+num = int(input())
+
+for i in range(num):
+    for j in range(num -i-1):
+        print(" ",end = " ")
+
+    for j in  range(i+1):
+        print("*",end = " ")
+    print()
