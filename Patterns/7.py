5dd3135c6c9be191bce8f5b97d7a10bbbf780ee3
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-08-11 22:56:11 +0530

    Added New Pattern

--- Patterns/7.py
+++ Patterns/7.py
@@ -34,3 +34,19 @@ for i in range(num):
         print("*",end = " ")    
 
     print()       
+
+# num = 3
+# num2 = 2 * num - 1
+
+# for i in range(num2):
+#     if i < num:
+#         for j in range(num - i - 1):
+#             print(" ", end=" ")
+#         for j in range(2 * i + 1):
+#             print("*", end=" ")
+#     else:
+#         for j in range(i - num + 1):
+#             print(" ", end=" ")
+#         for j in range(2 * (num2 - i - 1) + 1):
+#             print("*", end=" ")
+#     print()
